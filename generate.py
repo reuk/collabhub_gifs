4ea9007825ee10375ee0e63cde1725a98c562b34
reuk <reuben.thomas@me.com>
2015-10-23 19:43:30 +0100

    added generate script

---
+++ generate.py
@@ -0,0 +1,17 @@
+import subprocess
+import os
+
+def main():
+    for i in ["collabhub_gif_0","collabhub_gif_1", "collabhub_gif_2", "collabhub_gif_3",]:
+        subprocess.call([
+            "convert",
+            "-loop",
+            "0",
+            "-delay",
+            "3",
+            os.path.join(i, "render", "*.png"),
+            os.path.join(i, "out.gif"),
+            ])
+
+if __name__ == "__main__":
+    main()
